69b31946b68430c520be54c31a495a234f33252d
Lucas Santiago Gomide <lucas.sgomide92@gmail.com>
2021-02-06 15:39:11 -0300

    Implementaçao de teste DB-Projetos, icones dinamicos, renderizacao dos cards de projeto

--- portfolio-react/src/App.js
+++ portfolio-react/src/App.js
@@ -2,12 +2,14 @@ import React from 'react';
 import './style/App.css';
 import Filters from './components/Filters';
 import Header from './components/Header';
+import ProjectList from './components/ProjectList';
 
 function App() {
   return (
     <div>
       <Header />
       <Filters />
+      <ProjectList />
     </div>
   );
 }
--- portfolio-react/src/components/Header.js
+++ portfolio-react/src/components/Header.js
@@ -1,5 +1,6 @@
 import React from 'react';
 import Button from './Button';
+import Icons from './ContactIcons';
 import headerButtons from '../data/headerButtons';
 import githubicon from '../style/images/icons/github-icon.png';
 import linkedinicon from '../style/images/icons/linkedin-icon.png';
@@ -7,18 +8,22 @@ import '../style/header.css';
 
 function Header() {
   const btnList = headerButtons;
-  const type = 'header-btn';
+  const TileName = "Meu Nome";
+  const bType = 'header-btn';
+  const iconEndpoint = 'LucasSGomide';
+  const altGitHub = 'github icon';
+  const altLinkedIn = 'linkedin icon';
 
   return (
     <nav className="header">
       <div className="left-side">
-        <h1>Meu Nome</h1>
+        <h1>{TileName}</h1>
       </div>
       <div className="right-side">
-        { btnList.map((btn) => <Button btnName={ btn } btnType={ type } key={ btn } />) }
+        { btnList.map((btn) => <Button btnName={ btn } btnType={ bType } key={ btn } />) }
         <div className="icons-box">
-          <img src={ githubicon } alt="github icon" className="icons" />
-          <img src={ linkedinicon } alt="linkedin icon" className="icons" />
+          <Icons source={ githubicon } user={ iconEndpoint } altTxt={ altGitHub } />
+          <Icons source={ linkedinicon } user={ iconEndpoint } altTxt={ altLinkedIn } />
         </div>
       </div>
     </nav>
