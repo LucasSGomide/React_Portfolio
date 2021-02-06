d8700934ad4c7bbcdde2b66f0c5df68daa3d0970
rafMewtwo <ponciano.raf@gmail.com>
2021-02-06 04:11:13 -0300

    css: background and another styles

--- portfolio-react/src/components/Header.js
+++ portfolio-react/src/components/Header.js
@@ -1,6 +1,8 @@
 import React from 'react';
 import Button from './Button';
 import headerButtons from '../data/headerButtons';
+import githubicon from '../style/images/icons/github-icon.png';
+import linkedinicon from '../style/images/icons/linkedin-icon.png';
 import '../style/header.css';
 
 function Header() {
@@ -9,7 +11,16 @@ function Header() {
 
   return (
     <nav className="header">
-      { btnList.map((btn) => <Button btnName={ btn } btnType={ type } key={ btn } />) }
+      <div className="left-side">
+        <h1>Meu Nome</h1>
+      </div>
+      <div className="right-side">
+        { btnList.map((btn) => <Button btnName={ btn } btnType={ type } key={ btn } />) }
+        <div className="icons-box">
+          <img src={ githubicon } alt="github icon" className="icons" />
+          <img src={ linkedinicon } alt="linkedin icon" className="icons" />
+        </div>
+      </div>
     </nav>
   );
 }
