ff283587eb7cc77e823ba4651dfa1687bea3fdce
rafMewtwo <ponciano.raf@gmail.com>
2021-02-07 16:32:06 -0300

    data, refatoracao and contatc

---
+++ portfolio-react/src/data/devInfo.js
@@ -0,0 +1,16 @@
+const devInfo = {
+  name: "Lucas Gomide",
+  age: 35,
+  whatsapp: "9281-7251",
+  description: {
+    resume: "Um breve resumo sobre o dev semelhante ao do linkedin",
+    fullDescription: "Uma descricao completa e detalhada do dev, contando sua historia e etc vou completar com  varios balbalablabalba para ter uma nocao de como vai ficar... ana hora da implementacao blbalbabblbalbabblbalbabblbalbabblbalbabblbalbabblbalbabblbalbabblbalbabblbalbabblbalbabblbalbabblbalbabblbalbabblbalbabblbalbabblbalbabblbalbabblbalbabblbalbabblbalbabblbalbabblbalbabblbalbabblbalbabblbalbabblbalbabblbalbabblbalbabblbalbabblbalbabblbalbabblbalbabblbalbabblbalbabblbalbabblbalbabblbalbabblbalbabblbalbabblbalbabblbalbabblbalbabblbalbabblbalbabblbalbabblbalbabblbalbabblbalbabblbalbabblbalbabblbalbabblbalbabblbalbab"
+  },
+  links: {
+    github: "https://www.github.com/in/",
+    linkedin: "https://www.linkedin.com/in/",
+    email: "assgyas@gmail.com"
+  }
+};
+
+export default devInfo;
