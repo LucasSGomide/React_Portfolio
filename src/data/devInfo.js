04063b3d1f1cdc16174999a5e71867ed75fca5fc
rafMewtwo <ponciano.raf@gmail.com>
2021-02-08 13:24:52 -0300

    att local folder

--- src/data/devInfo.js
+++ src/data/devInfo.js
@@ -7,7 +7,7 @@ const devInfo = {
     fullDescription: "Uma descricao completa e detalhada do dev, contando sua historia e etc vou completar com  varios balbalablabalba para ter uma nocao de como vai ficar... ana hora da implementacao blbalbabblbalbabblbalbabblbalbabblbalbabblbalbabblbalbabblbalbabblbalbabblbalbabblbalbabblbalbabblbalbabblbalbabblbalbabblbalbabblbalbabblbalbabblbalbabblbalbabblbalbabblbalbabblbalbabblbalbabblbalbabblbalbabblbalbabblbalbabblbalbabblbalbabblbalbabblbalbabblbalbabblbalbabblbalbabblbalbabblbalbabblbalbabblbalbabblbalbabblbalbabblbalbabblbalbabblbalbabblbalbabblbalbabblbalbabblbalbabblbalbabblbalbabblbalbabblbalbabblbalbabblbalbab"
   },
   links: {
-    github: "https://www.github.com/in/",
+    github: "https://www.github.com/rafMewtwo",
     linkedin: "https://www.linkedin.com/in/",
     email: "assgyas@gmail.com"
   }
